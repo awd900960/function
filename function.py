59b4d92e7b2732a0dcf67ac45bdeba3b70be735c
awd900960 <awd900960@gmail.com>
2022-05-29 13:59:37 +0800

    first commit

---
+++ function.py
@@ -0,0 +1,9 @@
+def is_leap_year(year):
+	    if (year % 400 == 0) or (year % 100 !=0 and year % 4 == 0 ):
+	    	 return Ture
+	    else:
+	         return False
+a = int (input('please input year'))
+print(is_leap_year(a))
+	            
+	
