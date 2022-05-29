59b9499a126526003f2a34dac398ddd4edde60f4
awd900960 <100074487+awd900960@users.noreply.github.com>
2022-05-29 14:05:45 +0800

    Update function.py

--- function.py
+++ function.py
@@ -1,7 +1,7 @@
 def is_leap_year(year):
 	    if (year % 400 == 0) or (year % 100 !=0 and year % 4 == 0):
-	    	   return  Ture
+	    	   return Ture
 	    else:
-	           return  False
+	           return False
 a = int (input('please input year'))
 print(is_leap_year(a))
